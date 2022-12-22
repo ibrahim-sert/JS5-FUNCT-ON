b2f17538cda70ca45824c1ddc9feffc3956959b1
ibrahim-sert <evo3506@gmail.com>
2022-12-22 22:33:25 +0300

    Functions starts

---
+++ app.js
@@ -0,0 +1,52 @@
+// function greet(firstName, lastName) {
+//   console.log(`Hi, ${firstName} ${lastName}`);
+// }
+
+// greet("Sabata", "2288");
+// greet("Sercan", "Yılmaz");
+// greet("İbarhim", "Sert");
+
+// const name = prompt("Your name:");
+// const surname = prompt("Your surname:");
+// greet(same, surname); //! JS' de fonsiyonlar parametrelere değişkenleri değil onların değerlerini aktarırlar
+
+// function calculate(name, YoB) {
+//   const age = 2022 - YoB; //? 2022 yerine new Date().getFullYear()
+//   console.log(`${name}'s age is ${age}`);
+// }
+
+// calculate("elif", 2000);
+
+// function topla(s1, s2) {
+//   const toplam = s1 + s2;
+//   console.log(toplam);
+// }
+
+// topla(2, 3);
+// console.log(x);
+
+// function oddOrEven(num) {
+//   return num % 2 ? "ODD" : "EVEN";
+// }
+
+// const number = Number(prompt("please enter a number to check odd or even"));
+// const oddEven = oddOrEven(number);
+// console.log(`${number} is ${oddEven}`);
+
+//?    YA DA
+
+// function oddOrEven(num) {
+//   return num % 2 === 0 ? "EVEN" : "ODD";
+// }
+
+// const number = Number(prompt("please enter a number to check odd or even"));
+// const oddEven = oddOrEven(number);
+// console.log(`${number} is ${oddEven}`);
+
+//?    YA DA
+
+const oddOrEven = function (num) {
+  return num % 2 === 0 ? "EVEN" : "ODD";
+};
+
+console.log(oddOrEven(5));
