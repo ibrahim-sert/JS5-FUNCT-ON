33c3a6f446992e3d9b4f65702e0c3866a92de13a
ibrahim-sert <evo3506@gmail.com>
2022-12-22 23:01:35 +0300

    CALCULATER ADDED

--- app.js
+++ app.js
@@ -62,4 +62,47 @@ const findBiggest = function (n1, n2, n3) {
   return biggest;
 };
 
+//*  BASİT HESAP MAKİNASI YAPMA
+
 console.log(`The Biggest:${findBiggest(2, 1, 4)}`);
+
+const add = function (n1, n2) {
+  return n1 + n2;
+};
+
+const sub = function (n1, n2) {
+  return n1 - n2;
+};
+const div = function (n1, n2) {
+  return n1 / n2;
+};
+const mul = function (n1, n2) {
+  return n1 * n2;
+};
+
+const compute = function (n1, n2, op) {
+  let result = 0;
+  switch (op) {
+    case "+":
+      result = add(n1, n2);
+      break;
+    case "-":
+      break;
+    case "/":
+      result = div(n1, n2);
+      break;
+
+    case "*":
+      result = mul(n1, n2);
+
+      break;
+
+    default:
+      break;
+  }
+  return result;
+};
+
+console.log(compute(4, 3, "/"));
+console.log(compute(2, 4, "*"));
+console.log(compute(3, 1, "/"));
